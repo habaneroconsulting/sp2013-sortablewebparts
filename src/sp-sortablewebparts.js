13c15586d39018e24543c7f3ceeaac991e51adf1
Christopher Parsons <cparsons@habaneroconsulting.com>
2013-10-17 17:31:41 -0700

    Cleaned up sortable script
    Edited comments and lines longer than 80 characters. Added window
    previous to various SP-related variables to fix linting.

--- src/sp-sortablewebparts.js
+++ src/sp-sortablewebparts.js
@@ -19,15 +19,16 @@
 			webparts: $(classes.webpart)
 		},
 		emptyWebpartId = 'MSOZone_EmptyZoneCell',
-		nextCell = classes.placeholder + ' + ' + classes.webpart + ', ' + classes.placeholder + ' + #' + emptyWebpartId;
+		nextCell = classes.placeholder + ' + ' + classes.webpart + ', ' +
+					classes.placeholder + ' + #' + emptyWebpartId;
 
 	/**
-	 * Add these two functions to the HTMLDivElement prototype since non-IE browsers
-	 * do not define them. We aren't actually recreating them like-for-like, just adding
-	 * in code to make our script work.
+	 * Add these two functions to the HTMLDivElement prototype since non-IE
+	 * browsers do not define them. We aren't actually recreating them
+	 * like-for-like, just adding in code to make our script work.
 	 */
 	module.prototypeSetup = function () {
-		// Updating this could break other uses of this call, but none were found
+		// Updating this could break other uses of this call, but none are found
 		HTMLDivElement.prototype.swapNode = function(original) {
 			var $original = $(original);
 
@@ -60,31 +61,35 @@
 		});
 
 		// On mouseup, we run the MoveWebPart function
-		// We need to use mouseup since using beforeStop (jQuery UI event) happens
-		// after the element is inserted in its new position. MoveWebPart requires
-		// the moving web part to be in its original spot, first.
+		// We need to use mouseup since using beforeStop (jQuery UI event)
+		// happens after the element is inserted in its new position.
+		// MoveWebPart requires the moving web part to be in its original spot,
+		// first.
 		elements.webparts.on('mouseup', function() {
-			// Check to see if the empty web part is previous to the dropped item
-			// This is needed since on empty web part zones, web parts that are moved into
-			// the zone are put below both the "Add a Web Part" and empty web part divs.
-			var placeholder = $(classes.placeholder)[0];
+			// Check to see if the empty web part is previous to the dropped
+			// item. This is needed since on empty web part zones, web parts
+			// that are moved into the zone are put below both the "Add a Web
+			// Part" and empty web part divs.
+			var placeholder = $(classes.placeholder)[0],
+				previouSibling = placeholder.previousElementSibling;
 
-			if (placeholder && placeholder.previousElementSibling.id === emptyWebpartId) {
-				placeholder.parentNode.insertBefore(placeholder, placeholder.previousElementSibling);
+			if (placeholder && previouSibling.id === emptyWebpartId) {
+				placeholder.parentNode.insertBefore(placeholder, previouSibling);
 			}
 
-			// Select the destination spot, which can be a web part, or an empty web part
+			// Select the destination spot, which can be a web part, or an empty
+			// web part
 			var destination = $(nextCell)[0];
 
 			if (destination) {
-				// These three global variables are set by the various drag and drop
-				// functions on the elements. We mock the values for now.
-				MSOLayout_zoneDragOver = elements.body[0];
-				MSOLayout_currentDragMode = 'move';
-				MSOLayout_iBar.setAttribute('goodDrop', true);
+				// These three global variables are set by the various drag and
+				// drop functions on the elements. We mock the values for now.
+				window.MSOLayout_zoneDragOver = elements.body[0];
+				window.MSOLayout_currentDragMode = 'move';
+				window.MSOLayout_iBar.setAttribute('goodDrop', true);
 
-				// This SP function re-orders the web parts and saves their position
-				MSOLayout_MoveWebPart(this, destination);				
+				// This function reorders the web parts and saves their position
+				window.MSOLayout_MoveWebPart(this, destination);				
 			}
 		});
 	};
@@ -101,7 +106,7 @@
 		inDesignMode = $('#MSOLayout_InDesignMode').val() === '1';
 		inWikiPageMode = $('#_wikiPageMode').val() === 'Edit';
 
-		// We are in edit mode if either design mode or wiki page mode are active
+		// We are in edit mode if either design or wiki page mode are active
 		inEditMode = (inDesignMode || inWikiPageMode);
 
 		return inEditMode;
@@ -113,7 +118,8 @@
 	 */
 	module.init = function () {
 		// If the ActiveX function dragDrop is not found, set up our script
-		if (HTMLDivElement.prototype.dragDrop === undefined && module.isEditMode()) {
+		if (HTMLDivElement.prototype.dragDrop === undefined &&
+			module.isEditMode()) {
 			// Check to see if the jQuery UI sortable function is available
 			if ($().sortable) {
 				module.setup();
